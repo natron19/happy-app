ccd4bcb4cd4ca10759815b720fa978ab89774ee9
Nathan R <nathan19@hotmail.com>
2015-01-25 15:51:04 -0800

    uploading changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,16 @@ class HomeController < ApplicationController
   before_action :set_joke, only: [:show, :edit, :update, :destroy]
 
   def index
-    @jokes = Joke.all
+    @result = ''
+    if params && params[:data]
+      if params[:data] == 'joke'
+        result = Joke.order('RANDOM()').first
+        @result = "#{result.setup} | #{result.punchline}"
+      elsif params[:data] == 'quote'
+        result = Quote.order('RANDOM()').first
+        @result = "#{result.saying} | #{result.source}"
+      end
+    end
   end
   
 end
